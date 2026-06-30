fc4e7ae1ccc7e29c1dd4f54590fac19ac478364a
unknown <st4537@utr.edu.mx>
2026-06-30 10:49:37 -0600

    coneection

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+require('dotenv').config();
 const express = require('express');
+const conectarDB = require("./src/config/database");
+conectarDB();
+
 const app = express();
 const port = 5100;
 
 app.listen(port, () => {
-    console.log("Hello world");
+    console.log("Servidor ejecutandose en el puerto");
 });
